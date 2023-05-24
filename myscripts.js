6814983f851fae1a7802fa0949f07ba42a301381
Abdullah <68245138+Abdullah1856@users.noreply.github.com>
2023-05-24 17:36:50 +0500

    Load more button code added

--- myscripts.js
+++ myscripts.js
@@ -24,3 +24,18 @@ $.each(lang, function (language, pourcent) {
     multiply++;
 
 });
+
+let loadMoreBtn = document.querySelector('#load-more');
+let currentItem = 3;
+
+loadMoreBtn.onclick = () =>{
+   let boxes = [...document.querySelectorAll('.sec-projects .container-projects .item-projects')];
+   for (var i = currentItem; i < currentItem + 3; i++){
+      boxes[i].style.display = 'inline-block';
+   }
+   currentItem += 3;
+
+   if(currentItem >= boxes.length){
+      loadMoreBtn.style.display = 'none';
+   }
+}
